4fb1503d3cc42580d77ef6001dd13a45fef15358
ryo <ryo.takeru7@yahoo.co.jp>
2020-03-30 23:49:43 +0900

    place new

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,11 +1,29 @@
 class PlacesController < ApplicationController
 
+    def index
+        @places = Place.all
+    end
+
     def show
         @place = Place.find(params[:id])
     end
 
-    def index
-        @places = Place.all
+    def new
+        @place = Place.new
     end
+
+    def create
+        @place = Place.new(place_params)
+        if @place.save
+            redirect_to @place
+        else
+            render 'new'
+        end
+    end
+
+    private
+        def place_params
+            params.require(:place).permit(:name, :menu, :intention_state)
+        end
 
 end
