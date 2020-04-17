84aee258b7e77e17e69ed0ce8217f39f55259b82
ryo <ryo.takeru7@yahoo.co.jp>
2020-04-18 00:48:31 +0900

    3

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,8 +6,8 @@ class UserController < ApplicationController
         #id = Join.find_by(user_id: @user.id) #ミス find,find_byは１つしか引っ張れない
         #@places = Place.find(@user.join_ids)
         id = current_user.place_ids
-        @places = Place.where("id IN (?)", id)
-        @places2 = Place.all
+        @places = Place.where("id IN (?)", id).order(etime: :DESC)
+        #@places2 = Place.all
 
         sanka_place = current_user.places.where("sanka_flg = (?)", 1)
         @sanka_place = sanka_place.order(etime: :DESC).first
