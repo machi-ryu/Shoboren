07e53fb82069159fffae9f2da36b5bf72d3315c8
ryo <ryo.takeru7@yahoo.co.jp>
2020-04-25 15:06:29 +0900

    5

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -93,15 +93,16 @@ $(function () {
             });
         }
     });
+});
 
 //タブ切り替え
+jQuery(function($){
     $('.tab').click(function(){
         $('.is-active').removeClass('is-active');
         $(this).addClass('is-active');
         $('.is-show').removeClass('is-show');
-        const index = $(this).index();
-        $('.panel').eq(index).addClass('is-show');
+        //const index = $(this).index();
+        //$('.panel').eq(index).addClass('is-show');
 
     });
-
 });
