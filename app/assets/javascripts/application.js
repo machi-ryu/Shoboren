8e76c810ca44113e4aae40b0a47a32af5b5a9eb3
ryo <ryo.takeru7@yahoo.co.jp>
2020-04-25 15:10:21 +0900

    7

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -101,8 +101,7 @@ jQuery(function($){
         $('.is-active').removeClass('is-active');
         $(this).addClass('is-active');
         $('.is-show').removeClass('is-show');
-        const index = $(this).index();
-        //$('.panel').eq(index).addClass('is-show');
-
+        let index = $(this).index();
+        $('.panel').eq(index).addClass('is-show');
     });
 });
